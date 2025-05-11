425acd9a64b61ee0fde2d409be436e1cfe9ceb27
P Hemanth Kumar <80844476+hemanth4134@users.noreply.github.com>
2025-05-11 23:22:37 +0530

    Update cicd_developer_backend-stack.ts

--- lib/cicd_developer_backend-stack.ts
+++ lib/cicd_developer_backend-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
