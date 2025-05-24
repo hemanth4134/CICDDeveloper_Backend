669e23b98bc60efe6b4696999d9b7f2082c8959f
hemanthp <hemanthp@godrej.com>
2025-05-24 09:38:01 +0530

    added

--- lib/cicd_developer_backend-stack.ts
+++ lib/cicd_developer_backend-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import * as sdk from 'aws-sdk';
+// import * as sdk from 'aws-sdk';
 import { Construct } from 'constructs';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
